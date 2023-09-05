d5dc549b5df2a69459440e02ac00917c13b7e687
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-09-05 16:20:55 +0200

    noop handler removed

--- logger-noop-handler/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-import com.swirlds.logging.api.extensions.handler.LogHandlerFactory;
-
-module com.swirlds.logging.handler.noop {
-    requires com.swirlds.logging.api;
-    requires com.swirlds.config;
-
-    provides LogHandlerFactory
-            with com.swirlds.logging.handler.noop.NoopHandlerFactory;
-}
