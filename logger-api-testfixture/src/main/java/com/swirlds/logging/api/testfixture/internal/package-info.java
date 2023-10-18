083f61e599132c794a7a97109a7d760ee5868355
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-10-18 10:07:37 +0200

    Javadoc

---
+++ logger-api-testfixture/src/main/java/com/swirlds/logging/api/testfixture/internal/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Internal classes for the implementation of the{@link com.swirlds.logging.api.testfixture.WithLoggingMirror}
+ * extension.
+ */
+package com.swirlds.logging.api.testfixture.internal;
